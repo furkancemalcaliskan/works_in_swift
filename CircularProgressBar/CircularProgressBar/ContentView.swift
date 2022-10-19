c7421f196413bbcee72c7698f300885c38b3bff0
Furkan Cemal Caliskan <furkanc.caliskan@icloud.com>
2022-10-19 16:06:25 +0300

    Added three circles

--- CircularProgressBar/CircularProgressBar/ContentView.swift
+++ CircularProgressBar/CircularProgressBar/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct ContentView: View {
     
     @State var firstCirclePercentage : Double = 0
+    @State var secondCirclePercentage : Double = 0
+    @State var thirdCirclePercentage : Double = 0
     
     var body: some View {
        
         ZStack {
             
-            Circular(lineWidth: 40, backgroundColor: Color.blue.opacity(0.2), foregroundColor: Color.blue, percentage: firstCirclePercentage)
+            Circular(lineWidth: 40, backgroundColor: Color.red.opacity(0.2), foregroundColor: Color.red, percentage: firstCirclePercentage)
                 .frame(width: 350,height: 350)
                 .onTapGesture {
                     if self.firstCirclePercentage == 0 {
@@ -25,6 +27,32 @@ struct ContentView: View {
                     } else {
                         
                         self.firstCirclePercentage = 0
+                        
+                    }
+                }
+            Circular(lineWidth: 40, backgroundColor: Color.green.opacity(0.2), foregroundColor: Color.green, percentage: secondCirclePercentage)
+                .frame(width: 250,height: 250)
+                .onTapGesture {
+                    if self.secondCirclePercentage == 0 {
+                        
+                        self.secondCirclePercentage = 75
+                        
+                    } else {
+                        
+                        self.secondCirclePercentage = 0
+                        
+                    }
+                }
+            Circular(lineWidth: 40, backgroundColor: Color.blue.opacity(0.2), foregroundColor: Color.blue, percentage: thirdCirclePercentage)
+                .frame(width: 150,height: 150)
+                .onTapGesture {
+                    if self.thirdCirclePercentage == 0 {
+                        
+                        self.thirdCirclePercentage = 100
+                        
+                    } else {
+                        
+                        self.thirdCirclePercentage = 0
                         
                     }
                 }
